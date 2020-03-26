c1172e011fc3339da45e6702b7c2d2e079007ac3
adamja <adam_289@hotmail.com>
2020-03-26 15:56:53 +0800

    Video 8 end - User Profile and Picture

--- django_project/users/apps.py
+++ django_project/users/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):  # required to be imported this way to avoid unwanted side effects (django documentation)
+        import users.signals
--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # If the user is deleted the profile will be deleted
+    image = models.ImageField(default='default.jpg', upload_to='profile_images')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
